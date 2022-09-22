c9de783cd5241c692c7201b32d973b89cbefb86a
JungInBaek <bji1527@naver.com>
2022-09-22 17:51:35 +0900

    day03 업로드
    JS 학습

---
+++ day03/js/jstest.js
@@ -0,0 +1,17 @@
+let product = 1000;
+let count = 5;
+console.log("물건 전체 가격 : " + product * count);
+
+let totalPrice = 1000;
+let price = 200;
+console.log("물건의 개수 : " + totalPrice / price);
+
+let apple = 2000;
+let pear = 3000;
+console.log(
+  "사과 구매 금액이 배 구매 금액보다 높을까? : " + (apple * 3 > pear * 2)
+);
+
+let yesterday = 25;
+let today = 10;
+console.log("어제와 오늘의 평균 온도는 : " + (yesterday + today) / 2);
